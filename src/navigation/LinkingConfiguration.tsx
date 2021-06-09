a4d406825b5b43bef4985f555055f08e8c1b8f1d
Gaspard Chevassus <chevassusgaspard@gmail.com>
2021-06-09 19:46:55 +0200

    Update LinkingConfiguration.tsx

--- src/navigation/LinkingConfiguration.tsx
+++ src/navigation/LinkingConfiguration.tsx
@@ -1,13 +1,15 @@
 import * as Linking from "expo-linking";
 
 export const SCREEN_ROUTES = {
-  Home: "home",
-  Links: "links"
+  Root: {
+    Home: "home",
+    Links: "links"
+  }
 };
 export default {
   prefixes: [Linking.makeUrl("/")],
   config: {
-    initialRouteName: SCREEN_ROUTES.Home,
+    initialRouteName: SCREEN_ROUTES.Root.Home,
     screens: SCREEN_ROUTES
   }
 };
